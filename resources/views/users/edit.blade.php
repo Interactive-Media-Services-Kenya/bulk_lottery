a8ab98cda79f7046525605cbc5ff6ff80011d685
SteveOwago <steveowago@gmail.com>
2022-11-23 14:57:13 +0300

    Changing User Perms to Auth on Edit ++++

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Add User</h3>
-                    <p class="text-subtitle text-muted">Add User Details</p>
+                    <p class="text-subtitle text-muted">Edit User Details</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
