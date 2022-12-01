9e79a561cffa48a436d4b36aaa424f4a9c9a3166
SteveOwago <steveowago@gmail.com>
2022-12-01 11:28:57 +0300

    Datatables Customer Transactions Today,week & month

--- app/Http/Controllers/TransactionCustomerController.php
+++ app/Http/Controllers/TransactionCustomerController.php
@@ -6,6 +6,7 @@ use App\Models\TransactionCustomer;
 use Illuminate\Http\Request;
 use DB;
 use Carbon\Carbon;
+use Yajra\DataTables\Facades\DataTables;
 
 class TransactionCustomerController extends Controller
 {
@@ -13,33 +14,151 @@ class TransactionCustomerController extends Controller
     {
         $this->middleware(['role_or_permission:Admin|transaction_customers_management']);
     }
-    public function index(){
-        $transactions = TransactionCustomer::orderBy('created_at', 'desc')->get();
-        $transactionsCustomer = $this->getCustomerTransactionStats();
-        return view('transactions.customers.index',compact('transactions','transactionsCustomer'));
-    }
-    public function today(){
-        $today =  Carbon::now()->today();
-        $transactions = TransactionCustomer::whereDate('created_at',$today)->orderBy('created_at', 'desc')->get();
-        $transactionsCustomer = $this->getCustomerTransactionStats();
-        return view('transactions.customers.today',compact('transactions','transactionsCustomer'));
-    }
-    public function week(){
-        $now = Carbon::now();
-        $weekStartDate = $now->startOfWeek()->format('Y-m-d H:i:s');
-        $weekEndDate = $now->endOfWeek()->format('Y-m-d H:i:s');
-        $month = Carbon::parse(Carbon::now()->today())->month;
-        $year = $now->year;
-        $transactions = TransactionCustomer::whereBetween('created_at',[$weekStartDate,$weekEndDate])->orderBy('created_at', 'desc')->get();
-        $transactionsCustomer = $this->getCustomerTransactionStats();
-        return view('transactions.customers.week',compact('transactions','transactionsCustomer'));
-    }
-    public function month(){
-        $month = Carbon::parse(Carbon::now()->today())->month;
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $query = TransactionCustomer::orderBy('created_at', 'desc');
 
-        $transactions = TransactionCustomer::whereMonth('created_at',$month)->orderBy('created_at', 'desc')->get();
+            $table = Datatables::of($query);
+
+
+            $table->editColumn('reference', function ($row) {
+                return $row->reference ? $row->reference : 'No Reference';
+            });
+
+            $table->editColumn('msisdn', function ($row) {
+                return $row->msisdn ? substr($row->msisdn, 0, 5) . '*****' . substr($row->msisdn, -2) : '';
+            });
+            $table->editColumn('mpesa_sender', function ($row) {
+                return $row->mpesa_sender ? $row->mpesa_sender : '';
+            });
+            $table->editColumn('mpesa_account', function ($row) {
+                return $row->mpesa_account ? $row->mpesa_account : '';
+            });
+            $table->editColumn('amount', function ($row) {
+                return $row->amount ? $row->amount : '';
+            });
+            $table->editColumn('created_at', function ($row) {
+                return $row->created_at ? $row->created_at : '';
+            });
+
+            $table->rawColumns(['reference', 'msisdn', 'mpesa_sender', 'mpesa_account', 'amount', 'created_at']);
+
+            return $table->make(true);
+        }
         $transactionsCustomer = $this->getCustomerTransactionStats();
-        return view('transactions.customers.month',compact('transactions','transactionsCustomer'));
+        return view('transactions.customers.index', compact( 'transactionsCustomer'));
+    }
+    public function today(Request $request)
+    {
+        if ($request->ajax()) {
+            $today =  Carbon::now()->today();
+            $query = TransactionCustomer::whereDate('created_at', $today)->orderBy('created_at', 'desc');
+
+            $table = Datatables::of($query);
+
+
+            $table->editColumn('reference', function ($row) {
+                return $row->reference ? $row->reference : 'No Reference';
+            });
+
+            $table->editColumn('msisdn', function ($row) {
+                return $row->msisdn ? substr($row->msisdn, 0, 5) . '*****' . substr($row->msisdn, -2) : '';
+            });
+            $table->editColumn('mpesa_sender', function ($row) {
+                return $row->mpesa_sender ? $row->mpesa_sender : '';
+            });
+            $table->editColumn('mpesa_account', function ($row) {
+                return $row->mpesa_account ? $row->mpesa_account : '';
+            });
+            $table->editColumn('amount', function ($row) {
+                return $row->amount ? $row->amount : '';
+            });
+            $table->editColumn('created_at', function ($row) {
+                return $row->created_at ? $row->created_at : '';
+            });
+
+            $table->rawColumns(['reference', 'msisdn', 'mpesa_sender', 'mpesa_account', 'amount', 'created_at']);
+
+            return $table->make(true);
+        }
+        $transactionsCustomer = $this->getCustomerTransactionStats();
+        return view('transactions.customers.today', compact( 'transactionsCustomer'));
+    }
+    public function week(Request $request)
+    {
+        if ($request->ajax()) {
+            $now = Carbon::now();
+            $weekStartDate = $now->startOfWeek()->format('Y-m-d H:i:s');
+            $weekEndDate = $now->endOfWeek()->format('Y-m-d H:i:s');
+            $month = Carbon::parse(Carbon::now()->today())->month;
+            $query = TransactionCustomer::whereBetween('created_at', [$weekStartDate, $weekEndDate])->orderBy('created_at', 'desc');
+
+            $table = Datatables::of($query);
+
+
+            $table->editColumn('reference', function ($row) {
+                return $row->reference ? $row->reference : 'No Reference';
+            });
+
+            $table->editColumn('msisdn', function ($row) {
+                return $row->msisdn ? substr($row->msisdn, 0, 5) . '*****' . substr($row->msisdn, -2) : '';
+            });
+            $table->editColumn('mpesa_sender', function ($row) {
+                return $row->mpesa_sender ? $row->mpesa_sender : '';
+            });
+            $table->editColumn('mpesa_account', function ($row) {
+                return $row->mpesa_account ? $row->mpesa_account : '';
+            });
+            $table->editColumn('amount', function ($row) {
+                return $row->amount ? $row->amount : '';
+            });
+            $table->editColumn('created_at', function ($row) {
+                return $row->created_at ? $row->created_at : '';
+            });
+
+            $table->rawColumns(['reference', 'msisdn', 'mpesa_sender', 'mpesa_account', 'amount', 'created_at']);
+
+            return $table->make(true);
+        }
+        $transactionsCustomer = $this->getCustomerTransactionStats();
+        return view('transactions.customers.week', compact('transactionsCustomer'));
+    }
+    public function month(Request $request)
+    {
+        if ($request->ajax()) {
+            $month = Carbon::parse(Carbon::now()->today())->month;
+            $query = TransactionCustomer::whereMonth('created_at', $month)->orderBy('created_at', 'desc');
+
+            $table = Datatables::of($query);
+
+
+            $table->editColumn('reference', function ($row) {
+                return $row->reference ? $row->reference : 'No Reference';
+            });
+
+            $table->editColumn('msisdn', function ($row) {
+                return $row->msisdn ? substr($row->msisdn, 0, 5) . '*****' . substr($row->msisdn, -2) : '';
+            });
+            $table->editColumn('mpesa_sender', function ($row) {
+                return $row->mpesa_sender ? $row->mpesa_sender : '';
+            });
+            $table->editColumn('mpesa_account', function ($row) {
+                return $row->mpesa_account ? $row->mpesa_account : '';
+            });
+            $table->editColumn('amount', function ($row) {
+                return $row->amount ? $row->amount : '';
+            });
+            $table->editColumn('created_at', function ($row) {
+                return $row->created_at ? $row->created_at : '';
+            });
+
+            $table->rawColumns(['reference', 'msisdn', 'mpesa_sender', 'mpesa_account', 'amount', 'created_at']);
+
+            return $table->make(true);
+        }
+        $transactionsCustomer = $this->getCustomerTransactionStats();
+        return view('transactions.customers.month', compact( 'transactionsCustomer'));
     }
     public function getCustomerTransactionStats()
     {
@@ -51,16 +170,16 @@ class TransactionCustomerController extends Controller
 
         $totalTransactions = DB::table('transaction_customers')->sum('amount');
         $transactionsToday = DB::table('transaction_customers')->whereDate('created_at', Carbon::now()->today())->sum('amount');
-        $transactionsWeek = DB::table('transaction_customers')->whereBetween('created_at',[$weekStartDate,$weekEndDate])->sum('amount');
-        $transactionsMonth = DB::table('transaction_customers')->whereMonth('created_at',$month)->sum('amount');
-        $transactionsYear = DB::table('transaction_customers')->whereYear('created_at',$year)->sum('amount');
+        $transactionsWeek = DB::table('transaction_customers')->whereBetween('created_at', [$weekStartDate, $weekEndDate])->sum('amount');
+        $transactionsMonth = DB::table('transaction_customers')->whereMonth('created_at', $month)->sum('amount');
+        $transactionsYear = DB::table('transaction_customers')->whereYear('created_at', $year)->sum('amount');
 
         $statistics = [
-            'totalTransactions'=> $totalTransactions,
-            'transactionsToday'=> $transactionsToday,
+            'totalTransactions' => $totalTransactions,
+            'transactionsToday' => $transactionsToday,
             'transactionsWeek' => $transactionsWeek,
-            'transactionsMonth'=> $transactionsMonth,
-            'transactionsYear'=> $transactionsYear
+            'transactionsMonth' => $transactionsMonth,
+            'transactionsYear' => $transactionsYear
         ];
 
         return $statistics;
--- resources/views/transactions/customers/month.blade.php
+++ resources/views/transactions/customers/month.blade.php
@@ -123,7 +123,7 @@
                             </tr>
                         </tbody>
                     </table>
-                    <table class="table" id="table1">
+                    <table class="table" id="TransactionTable">
                         <thead>
                             <tr>
                                 <th>TransactionID</th>
@@ -135,7 +135,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @forelse ($transactions as $item)
+                            {{-- @forelse ($transactions as $item)
                                 <tr>
                                     <td>{{ $item->reference }}</td>
                                     <td>{{ substr($item->msisdn, 0, 5) . '*****' . substr($item->msisdn, -2) }}</td>
@@ -148,7 +148,7 @@
                                 <tr>
                                     <td colspan="6" class="text-center">No Transactions</td>
                                 </tr>
-                            @endforelse
+                            @endforelse --}}
 
                         </tbody>
                     </table>
@@ -159,10 +159,91 @@
         <!-- Basic Tables end -->
     @endsection
     @section('scripts')
+
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
         <script src="https://cdn.datatables.net/datetime/1.2.0/js/dataTables.dateTime.min.js"></script>
+        <!-- Script Section  -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
+            integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.colVis.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#TransactionTable').DataTable({
+                    processing: true,
+                    method: 'GET',
+                    serverSide: true,
+                    ajax: "{{ route('transactions.customers.month') }}",
+                    columns: [{
+                            data: 'reference',
+                            name: 'reference'
+                        },
+
+                        {
+                            data: 'msisdn',
+                            name: 'msisdn'
+                        },
+                        {
+                            data: 'mpesa_sender',
+                            name: 'mpesa_sender'
+                        },
+                        {
+                            data: 'mpesa_account',
+                            name: 'mpesa_account'
+                        },
+                        {
+                            data: 'amount',
+                            name: 'amount'
+                        },
+                        {
+                            data: 'created_at',
+                            name: 'created_at'
+                        },
+                    ],
+                    dom: 'lBfrtip',
+                    pageLength: 100,
+                    buttons: [
+                        'copy',
+                        {
+                            extend: 'excelHtml5',
+                            title: 'Contact_list',
+                            exportOptions: {
+                                exportOptions: {
+                                    columns: [0, 1, 2, 3, ':visible']
+                                }
+                            }
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            title: 'Contact_list',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3]
+                            }
+                        },
+                        'colvis'
+                    ]
+                });
+            });
+        </script>
         <script>
             var minDate, maxDate;
 
@@ -195,7 +276,7 @@
                 });
 
                 // DataTables initialisation
-                var table = $('#table1').DataTable();
+                var table = $('#TransactionTable').DataTable();
 
                 // Refilter the table
                 $('#min, #max').on('change', function() {
@@ -204,3 +285,4 @@
             });
         </script>
     @endsection
+
