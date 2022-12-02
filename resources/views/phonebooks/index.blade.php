07cdf3848ca2b8b82409a1010c617c98eb86248b
SteveOwago <steveowago@gmail.com>
2022-12-02 16:25:40 +0300

    Changing Scripts for Ajax datatables

--- resources/views/phonebooks/index.blade.php
+++ resources/views/phonebooks/index.blade.php
@@ -5,44 +5,45 @@
     <link rel="stylesheet" href="assets/css/pages/datatables.css">
 @endsection
 @section('content')
-<div class="page-heading">
-    <div class="page-title">
-        <div class="row">
-            <div class="col-12 col-md-6 order-md-1 order-last">
-                <h3>All PhoneBooks</h3>
-                <p class="text-subtitle text-muted">Click on PhoneBook Name to View Entire Details</p>
-            </div>
-            <div class="col-12 col-md-6 order-md-2 order-first">
-                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">PhoneBooks</li>
-                    </ol>
-                </nav>
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>All PhoneBooks</h3>
+                    <p class="text-subtitle text-muted">Click on PhoneBook Name to View Entire Details</p>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">PhoneBooks</li>
+                        </ol>
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Basic Tables start -->
-    <section class="section">
-        <div class="card">
-            <div class="card-header">
-                PhoneBooks
-                <a href="{{route('phonebooks.create')}}" class="btn btn-primary float-start float-lg-end">Add PhoneBook</a>
-            </div>
-            <div class="card-body">
-                <table class="table" id="PhoneBookTable">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Company Name</th>
-                            <th>No. Contacts</th>
-                            <th>Date Created</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {{-- @forelse ($phoneBooks as $item)
+        <!-- Basic Tables start -->
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    PhoneBooks
+                    <a href="{{ route('phonebooks.create') }}" class="btn btn-primary float-start float-lg-end">Add
+                        PhoneBook</a>
+                </div>
+                <div class="card-body">
+                    <table class="table" id="PhoneBookTable">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Company Name</th>
+                                <th>No. Contacts</th>
+                                <th>Date Created</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @forelse ($phoneBooks as $item)
                         <tr>
                             <td>{{$item->name}}</td>
                             <td>{{$item->client->name}}</td> --}}
@@ -65,95 +66,71 @@
                         </tr>
                         @endforelse --}}
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
 
-    </section>
-    <!-- Basic Tables end -->
-@endsection
-@section('scripts')
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
-<script src="https://cdn.datatables.net/datetime/1.2.0/js/dataTables.dateTime.min.js"></script>
-<!-- Script Section  -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-    integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
-    integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.colVis.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#PhoneBookTable').DataTable({
-            aaSorting: [[3, "desc"]],
-            processing: true,
-            method: 'GET',
-            serverSide: true,
-            ajax: "{{ route('phonebooks.index') }}",
-            columns: [{
-                    data: 'name',
-                    name: 'name'
-                },
+        </section>
+        <!-- Basic Tables end -->
+    @endsection
+    @section('scripts')
+        @include('layouts.partials.utilities.datatableScripts')
+        <script>
+            $(document).ready(function() {
+                $('#PhoneBookTable').DataTable({
+                    aaSorting: [
+                        [3, "desc"]
+                    ],
+                    processing: true,
+                    method: 'GET',
+                    serverSide: true,
+                    ajax: "{{ route('phonebooks.index') }}",
+                    columns: [{
+                            data: 'name',
+                            name: 'name'
+                        },
 
-                {
-                    data: 'client',
-                    name: 'client.name'
-                },
-                {
-                    data: 'contacts',
-                    name: 'contacts_count'
-                },
-                {
-                    data: 'created_at',
-                    name: 'created_at'
-                },
-                {
-                    data: 'actions',
-                    name: 'actions'
-                },
-            ],
-            dom: 'lBfrtip',
-            pageLength: 100,
-            buttons: [
-                'copy',
-                {
-                    extend: 'excelHtml5',
-                    title: 'Contact_list',
-                    exportOptions: {
-                        exportOptions: {
-                            columns: [0, 1, 2, 3, ':visible']
-                        }
-                    }
-                },
-                {
-                    extend: 'pdfHtml5',
-                    title: 'Contact_list',
-                    exportOptions: {
-                        columns: [0, 1, 2, 3]
-                    }
-                },
-                'colvis'
-            ]
-        });
-    });
-</script>
-@endsection
-
+                        {
+                            data: 'client',
+                            name: 'client.name'
+                        },
+                        {
+                            data: 'contacts',
+                            name: 'contacts_count'
+                        },
+                        {
+                            data: 'created_at',
+                            name: 'created_at'
+                        },
+                        {
+                            data: 'actions',
+                            name: 'actions'
+                        },
+                    ],
+                    dom: 'lBfrtip',
+                    pageLength: 100,
+                    buttons: [
+                        'copy',
+                        {
+                            extend: 'excelHtml5',
+                            title: 'Contact_list',
+                            exportOptions: {
+                                exportOptions: {
+                                    columns: [0, 1, 2, 3, ':visible']
+                                }
+                            }
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            title: 'Contact_list',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3]
+                            }
+                        },
+                        'colvis'
+                    ]
+                });
+            });
+        </script>
+    @endsection
--- resources/views/transactions/customers/month.blade.php
+++ resources/views/transactions/customers/month.blade.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
             @if (auth()->user()->hasrole('Admin') ||
-                    auth()->user()->can('transaction_customers_management'))
+                auth()->user()->can('transaction_customers_management'))
                 <div class="row">
                     <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
@@ -37,8 +37,11 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Total Transctions</h6>
-                                        <a href="{{route('transactions.customers.index')}}"><h6 class="font-extrabold mb-0">KES {{ $transactionsCustomer['totalTransactions'] }}
-                                        </h6></a>
+                                        <a href="{{ route('transactions.customers.index') }}">
+                                            <h6 class="font-extrabold mb-0">KES
+                                                {{ $transactionsCustomer['totalTransactions'] }}
+                                            </h6>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -55,8 +58,11 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Today Transactions</h6>
-                                        <a href="{{route('transactions.customers.index')}}"><h6 class="font-extrabold mb-0">KES {{ $transactionsCustomer['transactionsToday'] }}
-                                        </h6></a>
+                                        <a href="{{ route('transactions.customers.index') }}">
+                                            <h6 class="font-extrabold mb-0">KES
+                                                {{ $transactionsCustomer['transactionsToday'] }}
+                                            </h6>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -73,8 +79,11 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Week Transactions</h6>
-                                        <a href="{{route('transactions.customers.week')}}"><h6 class="font-extrabold mb-0">KES {{ $transactionsCustomer['transactionsWeek'] }}
-                                        </h6></a>
+                                        <a href="{{ route('transactions.customers.week') }}">
+                                            <h6 class="font-extrabold mb-0">KES
+                                                {{ $transactionsCustomer['transactionsWeek'] }}
+                                            </h6>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -91,8 +100,11 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Month Transactions</h6>
-                                        <a href="{{route('transactions.customers.month')}}"><h6 class="font-extrabold mb-0">KES {{ $transactionsCustomer['transactionsMonth'] }}
-                                        </h6></a>
+                                        <a href="{{ route('transactions.customers.month') }}">
+                                            <h6 class="font-extrabold mb-0">KES
+                                                {{ $transactionsCustomer['transactionsMonth'] }}
+                                            </h6>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -159,37 +171,13 @@
         <!-- Basic Tables end -->
     @endsection
     @section('scripts')
-
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
-        <script src="https://cdn.datatables.net/datetime/1.2.0/js/dataTables.dateTime.min.js"></script>
-        <!-- Script Section  -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
-            integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.colVis.min.js"></script>
+        @include('layouts.partials.utilities.datatableScripts')
         <script>
             $(document).ready(function() {
                 $('#TransactionTable').DataTable({
-                    aaSorting: [[5, "desc"]],
+                    aaSorting: [
+                        [5, "desc"]
+                    ],
                     processing: true,
                     method: 'GET',
                     serverSide: true,
@@ -286,4 +274,3 @@
             });
         </script>
     @endsection
-
